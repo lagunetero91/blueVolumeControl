c86aba89853dd6cfa8e519d671fb99725aa6ceeb
Laura <l4depende@gmail.com>
2017-01-23 20:42:06 +0100

    Base project

---
+++ swift_host/BlueVolumeControl/BlueVolumeControl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BlueVolumeControl
+//
+//  Created by Laura del Pino Díaz on 23/1/17.
+//  Copyright © 2017 Laura del Pino Díaz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
